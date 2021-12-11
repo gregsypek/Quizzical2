f3c96d3bf59111e0ff36fbc66da8907596601f7e
greg <greg.sypek@gmail.com>
2021-12-11 17:27:05 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from "react";
 import Home from "./Home";
+import Check from "./Check";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="wrapper grid bg-white">
-			<Home />
+			<BrowserRouter>
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route exact path="/check" element={<Check />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
---
+++ src/Check.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Check() {
+	return <h1>Jestem check</h1>;
+}
